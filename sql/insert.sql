e3b4eebecc05d42b87ecfb0fcafc8129876b75c4
Etu1906 <118957897+Etu1906@users.noreply.github.com>
2023-09-28 22:40:57 +0100

    new data

--- sql/insert.sql
+++ sql/insert.sql
@@ -18,13 +18,7 @@ VALUES
     (2, 'Poste 3'),
     (2, 'Poste 4');
 
--- Insertion de données dans la table "besoin"
-INSERT INTO besoin (idposte, heurepersonne , heuresemaine , accompli)
-VALUES
-    (1, 40.0, 100.0, 1),
-    (1, 35.0, 90.0, 1),
-    (2, 38.0, 110.0, 1),
-    (3, 30.0, 80.0, 1);
+
 
 
 -- ito ny vaovao
@@ -65,11 +59,10 @@ VALUES
     (5, 'en couple' )
     ;
 
-
-
-
-
-
-
-
-
+-- Insertion de données dans la table "besoin"
+INSERT INTO besoin (idposte, heurepersonne , heuresemaine , accompli)
+VALUES
+    (1, 40.0, 100.0, now()),
+    (1, 35.0, 90.0, now()),
+    (2, 38.0, 110.0, null),
+    (3, 30.0, 80.0, null);
