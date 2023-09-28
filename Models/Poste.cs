10c9fb7b221cc52a49fa1e38837e8a1c0acd3c37
Rebeka-1892 <etu1892@gmail.com>
2023-09-28 22:18:54 +0200

    poste

--- Models/Poste.cs
+++ Models/Poste.cs
@@ -1,4 +1,7 @@
 namespace RH.Models;
+using System;
+using System.Collections.Generic;
+using Npgsql;
 
 public class Poste{
     int _idPoste;
@@ -17,6 +20,41 @@ public class Poste{
         set { _nomPoste = value; }
     }
 
+    public static Poste[] GetAll(NpgsqlConnection npg, int ids) {
+        Poste[] postes = null;
+        bool estOuvert = false;
+        
+        if (npg == null)        {
+            estOuvert = true;
+            Connection connexion = new Connection();
+            npg = connexion.ConnectSante();
+        }        
+        try        {
+            string sql = "SELECT * FROM poste where idservice=" + ids;
+            Console.WriteLine(sql);         
+            Service service = new(ids);
+            using (NpgsqlCommand command = new NpgsqlCommand(sql, npg))            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())                {
+                    List<Poste> posteList = new List<Poste>();
+                    while (reader.Read())                    {
+                        Poste poste = new(reader.GetInt32(0), service, reader.GetString(2));
+                        posteList.Add(poste);
+                    }
+                    postes = posteList.ToArray();
+                }
+            }
+        }
+        catch (Exception e)        {
+            Console.WriteLine(e.ToString());
+        }
+        finally        {
+            if (estOuvert)            {
+                npg.Close();
+            }
+        }      
+        Console.WriteLine(postes);  
+        return postes;
+    }
    
     public Poste() {}
 
@@ -25,4 +63,9 @@ public class Poste{
         this._service = serv;
         this._nomPoste = np;
     }   
+    
+    public Poste(Service serv, string np){
+    this._service = serv;
+    this._nomPoste = np;
+} 
 }
