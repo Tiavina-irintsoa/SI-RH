bdb3809d139b0350cb3b456485f2a05089c3459a
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2023-10-08 20:08:49 +0200

    Ajout onload (create form test)

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,55 +3,79 @@ using Newtonsoft.Json;
 using RH.Models; 
 namespace RH.Controllers{
     public class TestController : Controller{
-    public IActionResult Liste()
-    {
-        HttpContext.Session.SetString( "test" , "bjr" );
-        return Redirect( "test" );
-    }
-    
-    public IActionResult createform()
-    {
-        return View( "Views/Home/CreateForm.cshtml" );
-    }
-
-    [HttpPost]
-public async Task<string> AddQuestion()
-{
-    try
-    {
-        using (StreamReader reader = new StreamReader(Request.Body))
+        
+        public IActionResult createform()
         {
-            string jsonBody = await reader.ReadToEndAsync();
-            QuestionData newQuestionData = JsonConvert.DeserializeObject<QuestionData>(jsonBody);
-            string question = newQuestionData.Question;
-            List<string> options = newQuestionData.Options;
+            return View( "Views/Home/CreateForm.cshtml" );
+        }
+
+        [HttpPost]
+        public async Task<string> AddQuestion()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body))
+                {
+                    string jsonBody = await reader.ReadToEndAsync();
+                    QuestionData newQuestionData = JsonConvert.DeserializeObject<QuestionData>(jsonBody);
+                    string question = newQuestionData.Question;
+                    List<string> options = newQuestionData.Options;
+                    var existingCookie = Request.Cookies["questionDataList"];
+                    List<QuestionData> questionDataList;
+                    if (string.IsNullOrEmpty(existingCookie))
+                    {
+                        questionDataList = new List<QuestionData>();
+                    }
+                    else
+                    {
+                        // Désérialisez la liste JSON depuis le cookie
+                        questionDataList = JsonConvert.DeserializeObject<List<QuestionData>>(existingCookie);
+                    }
+                    questionDataList.Add(newQuestionData);
+                    var updatedCookieValue = JsonConvert.SerializeObject(questionDataList);
+                    Response.Cookies.Append("questionDataList", updatedCookieValue, new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddDays(1)
+                    });
+                    return JsonConvert.SerializeObject(questionDataList);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return e.Message;
+            }
+        }
+
+        public IActionResult Save(){
             var existingCookie = Request.Cookies["questionDataList"];
             List<QuestionData> questionDataList;
             if (string.IsNullOrEmpty(existingCookie))
             {
-                questionDataList = new List<QuestionData>();
+                return RedirectToAction("createform");
             }
-            else
-            {
-                // Désérialisez la liste JSON depuis le cookie
-                questionDataList = JsonConvert.DeserializeObject<List<QuestionData>>(existingCookie);
-            }
-            questionDataList.Add(newQuestionData);
-            var updatedCookieValue = JsonConvert.SerializeObject(questionDataList);
-            Response.Cookies.Append("questionDataList", updatedCookieValue, new CookieOptions
-            {
-                Expires = DateTime.Now.AddDays(1)
-            });
-            return JsonConvert.SerializeObject(questionDataList);
-        }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-        return e.Message;
-    }
-}
+            questionDataList = JsonConvert.DeserializeObject<List<QuestionData>>(existingCookie);
+            
+            
 
+        }
+        [HttpGet]
+        public async Task<string> GetAll(){
+            try
+            {
+                string existingCookie = Request.Cookies["questionDataList"];
+                string questionDataList = "";
+                if (string.IsNullOrEmpty(existingCookie) ==  false)
+                {
+                    questionDataList = existingCookie;
+                }
+                return questionDataList;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
         
     }
 }
