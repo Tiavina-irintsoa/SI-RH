7fccd373bc9ad730d5fb2b77967cdc61dd32678b
Etu1906 <118957897+Etu1906@users.noreply.github.com>
2023-10-19 13:52:34 +0400

    utilitaire hoan masiso

--- Models/Utilitaire.cs
+++ Models/Utilitaire.cs
@@ -2,6 +2,55 @@ using Npgsql;
 namespace RH.Models
 {
     public class Utilitaire{
+        public static double  CalculateHoursDifference( string dt1 , string tm1 , string dt2 , string tm2 ){
+            return CalculateHoursDifference( toDateTime( dt1 , tm1 ) , toDateTime( dt2 , tm2 )  );
+        }
+        
+        public static double CalculateHoursDifference(DateTime dateTime1, DateTime dateTime2)
+        {
+            TimeSpan timeDifference = dateTime2 - dateTime1;
+            return timeDifference.TotalHours;
+        }
+        public static DateTime toDateTime( string dateStr1 , string timeStr1  )
+        {
+            DateTime dateTime1 = DateTime.Parse(dateStr1 + " " + timeStr1);
+            return dateTime1;
+        }
+        public static bool IsDateGreaterThan15DaysFromToday(string dateString)
+        {
+            if (DateTime.TryParseExact(dateString, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out DateTime inputDate))
+            {
+                DateTime today = DateTime.Today;
+                Console.WriteLine( inputDate+ " - " +today );
+                TimeSpan difference = inputDate - today;
+                Console.WriteLine( "la difference est : "+difference );
+                if( difference.TotalDays < 0 ){
+                    throw new Exception( " cette date n'est plus disponible " );
+                }
+                if (difference.TotalDays > 15)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static List<string> ConvertStringToList(string input)
+        {
+            string[] parts = input.Split(',');
+            return new List<string>(parts);
+        }
+
+        public  static string GenerateCondition(string fieldName, List<string> values)
+        {
+            if (values.Count == 0)
+            {
+                return ""; // Si la liste est vide, retourner une chaîne vide
+            }
+
+            string condition = string.Join(" or ", values.Select(value => $"{fieldName} = {value}"));
+            return condition;
+        }
         public static int getLast(NpgsqlConnection npg, string colonne , string table)
         {
             int nextSerialValue = 0;
