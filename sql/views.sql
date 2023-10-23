32c4a52872d1377d47f1dd5248025274361506bf
Rebeka-1892 <etu1892@gmail.com>
2023-10-23 08:48:22 +0200

    vu

--- sql/views.sql
+++ sql/views.sql
@@ -194,6 +194,13 @@ create or replace view v_service_poste as
     from poste as p
         natural join service as  s;
 
-    
+create or replace view v_essai_fin as(
+    SELECT *
+    FROM contrat_essai
+    NATURAL JOIN essai
+    NATURAL JOIN candidat
+    WHERE (debut::date + (duree || ' month')::interval) <= current_date
+    AND idcontrat_essai NOT IN (SELECT idcontrat_essai FROM travail)
+);
 
 
