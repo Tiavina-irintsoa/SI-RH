846488ed88424ef5ee41d5e58593aa610c57e90c
Tiavina-irintsoa <rakotonirinairintsoa0@gmail.com>
2023-10-23 10:33:26 +0200

    heure supp validation

--- Models/HeureSupp.cs
+++ Models/HeureSupp.cs
@@ -6,9 +6,16 @@ namespace RH.Models{
         public DateTime debut {get;set;}
         public DateTime fin {get;set;}
         public Service? Service {get;set;}
+
         public List<Personnel>? Personnels {get;set;}
         public string? Raison {get;set;}
-
+        public HeureSupp(int idservice, string nomservice, string icone,TimeSpan debut, TimeSpan fin, DateTime date, int idHeureSupp ,string raison){   
+            Service = new Service(idservice,nomservice,icone);
+            this.debut = date + debut;
+            this.fin = date + fin; 
+            this.IdHeureSup = idHeureSupp;
+            Raison =raison;
+        }
         public void setPersonnels(List<int> ids){
             Personnels = new List<Personnel>();
             foreach (var id in ids)
@@ -16,6 +23,163 @@ namespace RH.Models{
                 Console.WriteLine(id);
                 Personnels.Add(new Personnel(id));
             }
+        }
+        public HeureSupp(){
+
+        }
+        public void setValidation(int newvalidation,NpgsqlConnection? npg){
+            bool estOuvert = false;
+
+        if (npg == null)
+        {
+            estOuvert = true;
+            Connection connexion = new Connection();
+            npg = connexion.ConnectSante();
+        }
+        try
+        {
+            string sql = "UPDATE demande_heure_sup SET validation = @newValidation WHERE iddemande_heure_sup = @idd";
+            Console.WriteLine(sql);
+            using (NpgsqlCommand command = new NpgsqlCommand(sql, npg))
+            {
+                command.Parameters.AddWithValue("@newValidation", newvalidation);
+                command.Parameters.AddWithValue("@idd", this.IdHeureSup);
+                int row = command.ExecuteNonQuery();
+                Console.WriteLine(row);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            throw;
+        }
+        finally
+        {
+            if (estOuvert)
+            {
+                npg.Close();
+            }
+        }
+        }
+        public string getDate(){
+            return debut.Date.ToString("yyyy-MM-dd");
+        }
+        public string getDebut(){
+            return debut.ToString("HH:mm");
+        }
+        public string getFin(){
+            return fin.ToString("HH:mm");
+        }
+        public HeureSupp(int id){
+            IdHeureSup = id;
+        }
+        public void complete(){
+            Personnels  = this.GetPersonnelHeureSup();
+            this.getHeureSuppById();
+        }
+        public List<Personnel> GetPersonnelHeureSup(){
+             List<Personnel> PersonnelList = new List<Personnel>();
+            using (NpgsqlConnection connection = new Connection().ConnectSante())
+            {
+                try
+                {
+                    string sql = "SELECT * FROM employe_demande_heure_sup where iddemande_heure_sup  = @id order by nom,nomposte";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("id", this.IdHeureSup);
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Personnel personnel = new Personnel
+                                (
+                                    Convert.ToString(reader["nom"]),
+                                    Convert.ToString(reader["prenom"]),
+                                    Convert.ToInt32(reader["idemploye"]),
+                                    Convert.ToString(reader["nomposte"])
+                                );
+                                PersonnelList.Add(personnel);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erreur lors de l'exécution de la requête : " + ex.Message);
+                    throw ex;
+                }
+            }
+            return PersonnelList;
+        }
+        public  void getHeureSuppById(){
+            
+            using (NpgsqlConnection connection = new Connection().ConnectSante())
+            {
+                try
+                {
+                    string sql = "SELECT * FROM v_heure_supp_non_consulte_avec_service where iddemande_heure_sup = @id order by date_heure_sup desc";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.AddWithValue("id",IdHeureSup);
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            reader.Read();
+                            
+                            Service = new Service(Convert.ToInt32(reader["idservice"]), Convert.ToString(reader["nomservice"]),Convert.ToString(reader["iconeservice"]));
+                            DateTime date = Convert.ToDateTime(reader["date_heure_sup"]);
+
+                            
+                            debut = date + TimeSpan.Parse(Convert.ToString(reader["heure_debut"]));
+                            fin = date + TimeSpan.Parse(Convert.ToString(reader["heure_fin"]));
+                                
+                            Raison  = Convert.ToString(reader["raison"]);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erreur lors de l'exécution de la requête : " + ex.Message);
+                    throw ex;
+                }
+            }
+            
+        }
+        public static List<HeureSupp> getHeureSupp(){
+            List<HeureSupp> HeureSuppList = new List<HeureSupp>();
+            using (NpgsqlConnection connection = new Connection().ConnectSante())
+            {
+                try
+                {
+                    string sql = "SELECT * FROM v_heure_supp_non_consulte_avec_service order by date_heure_sup desc";
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(sql, connection))
+                    {
+                        using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                HeureSupp HeureSupp = new HeureSupp
+                                (
+                                    Convert.ToInt32(reader["idservice"]),
+                                    Convert.ToString(reader["nomservice"]),
+                                    Convert.ToString(reader["iconeservice"]),
+                                    TimeSpan.Parse(Convert.ToString(reader["heure_debut"])),
+                                    TimeSpan.Parse(Convert.ToString(reader["heure_fin"])),
+                                    Convert.ToDateTime(reader["date_heure_sup"]),
+                                    Convert.ToInt32(reader["iddemande_heure_sup"]),
+                                    Convert.ToString(reader["raison"])
+                                );
+                                HeureSuppList.Add(HeureSupp);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erreur lors de l'exécution de la requête : " + ex.Message);
+                    throw ex;
+                }
+            }
+            return HeureSuppList;
         }
         public void InsererHeureSup(){
         using (NpgsqlConnection connection =new Connection(). ConnectSante())
@@ -64,6 +228,7 @@ namespace RH.Models{
         }
     }
 
+        
         public List<Personnel> GetPersonnelByServiceId()
         {
             List<Personnel> personnelList = new List<Personnel>();
